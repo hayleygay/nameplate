dfed6d797d9f4f31fec927e1b9970ae67b60d8e4
hayleygay <hgay@eagles.ewu.edu>
2016-10-18 12:57:22 -0700

    Version 2

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// My Scripts
+
+//Lettering
+$('h1').lettering();
+
+//Backstretch
+$.backstretch('img/bg.jpg');
+
+//Masonry
+$('.grid').masonry({
+  itemSelector: '.grid-item',
+  columnWidth: 200
+});
